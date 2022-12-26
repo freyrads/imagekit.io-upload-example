56b017def7548e4b0bf8408a456325763de1079d
frs18840@gmail.com <frs18840@gmail.com>
2022-12-26 20:48:03 +0700

    add upload result example

--- app.js
+++ app.js
@@ -11,23 +11,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-/*
-
-Example req.file
-{
-  fieldname: 'media',
-  originalname: 'JFvdYo_N0TQ3Vis20IDTbe8lsEFP.webp',
-  encoding: '7bit',
-  mimetype: 'image/webp',
-  destination: '/home/uploads',
-  filename: 'b963f18fb79739f0cdfa1156d706fe14',
-  path: '/home/uploads/b963f18fb79739f0cdfa1156d706fe14',
-  size: 5824
-}
-
-*/
-
 app.post("/", upload.single("media"), async (req, res, next) => {
+  /*
+
+  Example req.file
+  {
+    fieldname: 'media',
+    originalname: 'JFvdYo_N0TQ3Vis20IDTbe8lsEFP.webp',
+    encoding: '7bit',
+    mimetype: 'image/webp',
+    destination: '/home/uploads',
+    filename: 'b963f18fb79739f0cdfa1156d706fe14',
+    path: '/home/uploads/b963f18fb79739f0cdfa1156d706fe14',
+    size: 5824
+  }
+  */
   console.log(req.file);
 
   const { fieldname, mimetype, filename, path } = req.file;
@@ -40,10 +38,24 @@ app.post("/", upload.single("media"), async (req, res, next) => {
     fileName : filename,
   });
 
+  /*
+
+    upload result example
+    {
+      fileId: '63a9a44de809dd54b0a149b7',
+      name: '28073b5a52f3aabfd33d66a71260f3a2_FjVw8ApjW',
+      size: 14268520,
+      versionInfo: { id: '63a9a44de809dd54b0a149b7', name: 'Version 1' },
+      filePath: '/28073b5a52f3aabfd33d66a71260f3a2_FjVw8ApjW',
+      url: 'https://ik.imagekit.io/imagekitid/28073b5a52f3aabfd33d66a71260f3a2_FjVw8ApjW',
+      fileType: 'non-image',
+      AITags: null
+    }
+  */
   console.log(ikRes);
 
   // save to database
-  // filename, 	  ikRes.url  		mimetype,  fieldname
+  // ikRes.name  ikRes.url   mimetype     fieldname
   // name:string,url:string,format:string,type:string
 
   console.log("Deleting uploaded file from server");
